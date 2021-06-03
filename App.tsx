a9c8a23359db15c455169553822245341f2d7d39
Oscar Monroy <oscarmonroy657@gmail.com>
2021-06-03 16:40:07 -0600

    Axios configured to work with the API.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { StackNavigation } from './src/navigation/StackNavigation';
 
 const App = () => {
   return (
     <NavigationContainer>
-
+      <StackNavigation/>
     </NavigationContainer>
   )
 }
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react'
+import { View } from 'react-native'
+import movieDb from '../api/movieDB';
+
+export const HomeScreen = () => {
+
+  useEffect(() => {}, []);
+  return (
+    <View>
+      
+    </View>
+  )
+}
