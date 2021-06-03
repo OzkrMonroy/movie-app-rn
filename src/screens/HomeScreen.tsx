5a70d8a29a43f2146d58ac5f14b8dc37540357fb
Oscar Monroy <oscarmonroy657@gmail.com>
2021-06-03 17:29:04 -0600

    The Spinner component has been created. Will be displayed when movies are not ready.

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,21 +1,20 @@
 import React from 'react'
-import { ActivityIndicator, View } from 'react-native'
+import { View } from 'react-native'
 import { useMovies } from '../hooks/useMovies'
+import { Spinner } from '../components/Spinner';
 
 export const HomeScreen = () => {
   const { moviesInTheaters, isLoading } = useMovies();
 
-  if(isLoading){
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignContent: 'center'}}>
-        <ActivityIndicator color='red' size={100}/>
-      </View>
-    )
-  }
-
   return (
     <View>
-      
+      {isLoading ? (
+        <Spinner/>
+      ) : (
+        <View>
+          
+        </View>
+      )}
     </View>
   )
 }
