10e316d1016aee8c074c6d10bb5deab514904cd6
Oscar Monroy <oscarmonroy657@gmail.com>
2021-06-05 21:01:50 -0600

    The package to use the Carousel into the app is ready. Carousel component is being used into HomeScreen.tsx

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import { View } from 'react-native'
+import { Dimensions, View } from 'react-native'
 import { useMovies } from '../hooks/useMovies'
 import { Spinner } from '../components/Spinner';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { MoviePoster } from '../components/MoviePoster';
+import Carousel from 'react-native-snap-carousel';
+
+const { width: windowWith } = Dimensions.get('window');
 
 export const HomeScreen = () => {
   const { moviesInTheaters, isLoading } = useMovies();
@@ -15,7 +18,13 @@ export const HomeScreen = () => {
         <Spinner/>
       ) : (
         <View style={{ marginTop: top }}>
-          <MoviePoster movie={moviesInTheaters[0]}/>
+          {/* <MoviePoster movie={moviesInTheaters[0]}/> */}
+          <Carousel
+            data={moviesInTheaters}
+            renderItem={({ item } : any) => <MoviePoster movie={item}/>}
+            sliderWidth={windowWith}
+            itemWidth={300}
+          />
         </View>
       )}
     </View>
